52adc3f51a6fedbefa298f570cf3bc60e965c1fa
shra114 <shravan.ashannagari@gmail.com>
2020-03-29 12:29:58 +0530

    Fix issues. Python3 compatible working fine

--- google_maps.py
+++ google_maps.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import datetime
+from functions import *
 
 
 def write_str_to_file_with_mode(str1,file1, mode, print_log):
@@ -8,19 +9,20 @@ def write_str_to_file_with_mode(str1,file1, mode, print_log):
     F.write(str1)
     F.close()
     if(print_log):
-        print "created the file ", file1
+        print ("created the file ", file1)
     return
 
 def get_time(html):
-	index_min = html.find('min\\"]')
-	index_min = html.find('min\\"]', index_min+3)
-	index_start = html.rfind('\\"', 0,index_min+3)
-	time = html[index_start+2:index_min+3]
-	return time
+    html = parse_file_as_str(html)
+    index_min = html.find('min\\')
+    index_min = html.find('min\\', index_min+3)
+    index_start = html.rfind('\\"', 0,index_min+3)
+    time = html[index_start+2:index_min+3]
+    return time
 
-url = "https://www.google.com/maps/dir/AMD+Research+%26+Development+Centre+India+Private+Limited,+8th+-11th+Floor,+Bldg.+No:11,+Raheja+Mindspace,+APIIC+Software+Layout,+Madhapur,+Hyderabad,+Telangana+500081,+India/G.P.Rao+Estates,+Rd+Number+2,+KPHB+Phase+I,+K+P+H+B+Phase+1,+Kukatpally,+Hyderabad,+Telangana+500072,+India/@17.4609282,78.3701546,14z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x3bcb93e23889a8b1:0x6eb581e2947a72ae!2m2!1d78.3821501!2d17.4380946!1m5!1m1!1s0x3bcb9192ff743d09:0x7191b8306899fbf4!2m2!1d78.398309!2d17.4898841!3e0" 
 
+url = "https://www.google.com/maps/dir/Hyderabad,+Telangana/Goa+Velha,+Goa+403110/@16.6660585,73.9542639,7z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x3bcb99daeaebd2c7:0xae93b78392bafbc2!2m2!1d78.486671!2d17.385044!1m5!1m1!1s0x3bbfbe8cf00e90a1:0x9c3d04969bd13976!2m2!1d73.9100068!2d15.501984!3e0"
 html = requests.get(url).content
-write_str_to_file_with_mode(html, "temp.html", "w", True)
-time = get_time(html)
-print time, "at ",datetime.datetime.now()
+write_str_to_file_with_mode(str(html), "temp.html", "w", True)
+time = get_time("temp.html")
+print (time, "at ",datetime.datetime.now())
